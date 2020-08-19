53c814d444e89dbd076e63e3c2fabce19176314f
ofitoussi1971 <55618215+ofitoussi1971@users.noreply.github.com>
2020-08-19 23:56:39 +0200

    Scripts 4 & 5 added by Olivier F

---
+++ script_4.js
@@ -0,0 +1,66 @@
+const entrepreneurs = [
+  { first: "Steve", last: "Jobs", year: 1955 },
+  { first: "Oprah", last: "Winfrey", year: 1954 },
+  { first: "Bill", last: "Gates", year: 1955 },
+  { first: "Sheryl", last: "Sandberg", year: 1969 },
+  { first: "Mark", last: "Zuckerberg", year: 1984 },
+  { first: "Beyonce", last: "Knowles", year: 1981 },
+  { first: "Jeff", last: "Bezos", year: 1964 },
+  { first: "Diane", last: "Hendricks", year: 1947 },
+  { first: "Elon", last: "Musk", year: 1971 },
+  { first: "Marissa", last: "Mayer", year: 1975 },
+  { first: "Walt", last: "Disney", year: 1901 },
+  { first: "Larry", last: "Page", year: 1973 },
+  { first: "Jack", last: "Dorsey", year: 1976 },
+  { first: "Evan", last: "Spiegel", year: 1990 },
+  { first: "Brian", last: "Chesky", year: 1981 },
+  { first: "Travis", last: "Kalanick", year: 1976 },
+  { first: "Marc", last: "Andreessen", year: 1971 },
+  { first: "Peter", last: "Thiel", year: 1967 }
+];
+
+// Réalise un script script_4.js qui affichera dans la console les questions suivantes et leur réponses :
+
+// Filtre dans cette liste les entrepreneurs qui sont nés dans les années 70 ;
+
+let seventies = [];
+for (let i = 0; i < entrepreneurs.length; i++) {
+  if (entrepreneurs[i].year >= 1970 && entrepreneurs[i].year < 1980) {
+    seventies.push(
+      entrepreneurs[i].first +
+        " " +
+        entrepreneurs[i].last +
+        ": " +
+        entrepreneurs[i].year
+    );
+  }
+}
+console.log(seventies);
+
+// Sors une array qui contient le prénom et le nom des entrepreneurs ;
+
+let fullNames = [];
+for (let i = 0; i < entrepreneurs.length; i++) {
+  fullNames.push(entrepreneurs[i].first + " " + entrepreneurs[i].last);
+}
+console.log(fullNames);
+
+// Quel âge aurait chaque inventeur aujourd'hui ?
+
+const currentYear = 2020;
+let entrepreneursAges = [];
+
+for (let i = 0; i < entrepreneurs.length; i++) {
+  entrepreneursAges.push(
+    entrepreneurs[i].first +
+      " " +
+      entrepreneurs[i].last +
+      ": " +
+      (currentYear - entrepreneurs[i].year)
+  );
+}
+console.log(entrepreneursAges);
+
+// Trie les entrepreneurs par ordre alphabétique du nom de famille.
+
+// Pas d'idée là, tout de suite ! :-D
---
+++ script_5.js
@@ -0,0 +1,67 @@
+const books = [
+  { title: "Gatsby le magnifique", id: 133712, rented: 39 },
+  { title: "A la recherche du temps,perdu", id: 237634, rented: 28 },
+  { title: "Orgueil & Préjugés", id: 873495, rented: 67 },
+  { title: "Les frères Karamazov", id: 450911, rented: 55 },
+  { title: "Dans les forêts de Sibérie", id: 8376365, rented: 15 },
+  { title: "Pourquoi j'ai mangé mon père", id: 450911, rented: 45 },
+  { title: "Et on tuera tous les affreux", id: 67565, rented: 36 },
+  { title: "Le meilleur des mondes", id: 88847, rented: 58 },
+  { title: "La disparition", id: 364445, rented: 33 },
+  { title: "La lune seule le sait", id: 63541, rented: 43 },
+  { title: "Voyage au centre de la Terre", id: 4656388, rented: 38 },
+  { title: "Guerre et Paix", id: 748147, rented: 19 }
+];
+
+// Réalise un script script_5.js qui affichera dans la console les questions suivantes et leurs réponses :
+
+// Est-ce que tous les livres ont été au moins empruntés une fois ?
+
+let unrentedBooks = [];
+for (let i = 0; i < books.length; i++) {
+  if (books[i].rented === 0) {
+    unrentedBooks.push("no");
+    console.log(unrentedBooks);
+  }
+}
+if (unrentedBooks.length === 0) {
+  console.log("Oui, tous les livres ont été loués au moins une fois !");
+} else {
+  console.log(
+    unrentedBooks.length + " livre(s) n'a (n'ont) jamais été loué(s)."
+  );
+}
+// Quel est le livre le plus emprunté ?
+
+books.sort(function(a, b) {
+  return b.rented - a.rented;
+});
+console.log("Le livre le plus emprunté est: " + books[0].title);
+
+// Quel est le livre le moins emprunté ?
+console.log("Le livre le moins emprunté est: " + books[books.length - 1].title);
+
+// Trouve le livre avec l'ID: 873495 ;
+
+let idFound = books.find(function(post, index) {
+  if (post.id == "873495") return true;
+});
+console.log(
+  "Le titre correspondant à l'ouvrage qui a comme ID",
+  idFound.id + " est : ",
+  idFound.title
+);
+
+// Supprime le livre avec l'ID: 133712 ;
+for (let i = 0; i < books.length; i++) {
+  if (books[i].id === 133712) {
+    books.splice(i, 1);
+  }
+}
+console.log("Voici l'array 'books' sans l'ID demandé :", books);
+
+// Trie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé).
+books.sort(function(a, b) {
+  return b.title - a.title;
+});
+console.log(books); // Marche pô !!!
